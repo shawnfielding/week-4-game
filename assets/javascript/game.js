3dc1bab21dd4bde1f653efe49ba36b20d83f7333
Shawn Fielding <shawnmfielding@gmail.com>
2017-03-03 17:47:05 -0500

    Final Version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,72 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var rand = function rand(low, high) {
+        console.log(low + typeof low + "and" + high+ typeof high);
+        
+        var newval = Math.floor(Math.random() * (high - low) + low);
+        return newval;
+    }
 
-var rand = function rand(low,high){
-    console.log (low+"and"+high);
-    var newval = ((Math.floor((Math.random()*high)+low)));
-    return newval
-  }
+    var currentVal = 0;
+    var totalWins = 0;
+    var totalLosses = 0;
+    var Goal = {
+        Val: 0,
+    };
+    var Characters = {
+        Cartman: {
+            Name: "Cartman",
+            Val: 0,
+        },
+        Stan: {
+            Name: "Stan",
+            Val: 0,
+        },
+        Kyle: {
+            Name: "Kyle",
+            Val: 0,
+        },
+        Kenny: {
+            Name: "Kenny",
+            Val: 0,
+        },
+    }
 
-  var currentVal = 0;
-  var totalWins = 0;
-  var totalLosses = 0;
-  var Goal = {
-    Val: 0,
-  };
+    function populatePage() {
+        if(currentVal > 0){
+            currentVal = 0
+        }
+        $("#CurrentNumber").html(currentVal);
+        $("#TotalWinsNumber").html(totalWins);
+        $("#TotalLossesNumber").html(totalLosses);
+        Goal.Val = rand(19,120);
+        $("#CurrentGoalNumber").html(Goal.Val);
+        $.each(Characters, function (ke, va) {
+            va.Val = rand(1,12);
+            $("."+va.Name).attr("id",va.Val);
+        });
+    }
+    
+        populatePage();
 
-  var Characters = {
-    Cartman: {
-      Name: "Cartman",
-      Img: "assets/images/cartman.jpg",
-      Val: 0,
-    },
-    Stan: {
-      Name: "Stan",
-      Img: "assets/images/stan.jpg",
-      Val: 0,
-    },
-    Kyle: {
-      Name: "Kyle",
-      Img: "assets/images/kyle.jpg",
-      Val: 0,
-    },
-    Kenny: {
-      Name: "Kenny",
-      Img: "assets/images/kenny.jpg",
-      Val: 0,
-    },
-  }
-
-function PopulatePage(){
-  if(currentVal > 0){
-    currentVal = 0
-  };
-  $("#CurrentNumber").html(currentVal);
-  $("#TotalWinsNumber").html(totalWins);
-  $("#TotalLossesNumber").html(totalLosses);
-  Goal.Val = rand(19,120);
-  $("#CurrentGoalNumber").html(Goal.Val);
-  $.each(Characters, function(ke,va){
-    va.Val = rand(1,12);
-    var contentstring = "<div class='col-xs-12 col-md-3 acharacter name='"+va.Name+"' id="+va.Val+"><img src='"+va.Img+"' class='character' /></div>";
-    $("#row3").append(contentstring);
-  });
-}
-
-PopulatePage();
-
-function TestDone(){
-  if(currentVal > Goal.Val){
-    alert("You Lose");
-    totalLosses = totalLosses + 1;
-    PopulatePage()
-  }else if(currentVal == Goal.Val){
-    alert("You Win")
-    totalWins = totalWins + 1;
-    PopulatePage()
-  }
-}
-
-  $(".acharacter").click(function(){
-    var theValue =($(this).attr('id'));
-    var theValue = parseInt(theValue);
-    currentVal = currentVal + theValue;
-    $("#CurrentNumber").html(currentVal);
-    TestDone();
-  });
+    function TestDone() {
+        if (currentVal > Goal.Val) {
+            alert("You Lose. You ended up with "+currentVal);
+            totalLosses = totalLosses + 1;
+            populatePage();
+        } else if (currentVal == Goal.Val) {
+            alert("You got " + Goal.Val + "! RIGHT ON! You Win! ")
+            totalWins = totalWins + 1;
+            populatePage();
+        }
+    };
+    $(".thecharacter").click(function() {
+        var theValue = ($(this).attr('id'));
+        console.log(this);
+        console.log(theValue);
+        var theValue = parseInt(theValue);
+        console.log(theValue);
+        currentVal = currentVal + theValue;
+        $("#CurrentNumber").html(currentVal);
+        TestDone();
+    });
 });
-// function TestDone(){
-//   if currentVal >
-//
-// }
